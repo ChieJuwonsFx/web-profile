5b576a256e44c62367ce9dfcbb03b6a4ac73e1af
Richie Olajuwon Santoso <richieolajuwons@gmail.com>
2025-01-07 19:13:41 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    build: {
-        outDir: 'dist',
-    },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'], // file input utama
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist', // pastikan output ke folder dist
+    emptyOutDir: true, // pastikan folder dist dibersihkan sebelum build
+    manifest: true, // buat manifest untuk mendeteksi file yang dihasilkan
+  },
 });
